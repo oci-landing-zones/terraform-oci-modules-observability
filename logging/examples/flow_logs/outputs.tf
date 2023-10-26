3422bb1192e8a5f30037b374a5e3627301200196
Khushboo Nigam <khushboo2243@gmail.com>
2023-10-26 14:13:09 -0400

    changes to module name

--- logging/examples/flow_logs/outputs.tf
+++ logging/examples/flow_logs/outputs.tf
@@ -2,13 +2,13 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 output "log_group" {
-  value = module.vision_logging.log_groups
+  value = module.flow_logging.log_groups
 }
 
 output "service_logs" {
-  value = module.vision_logging.service_logs
+  value = module.flow_logging.service_logs
 }
 
 output "custom_logs" {
-  value = module.vision_logging.custom_logs
+  value = module.flow_logging.custom_logs
 }
