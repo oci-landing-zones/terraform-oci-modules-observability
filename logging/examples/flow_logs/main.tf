50b1ec3dff65e459a0da2e3b808681a9e86e18c7
Khushboo Nigam <khushboo2243@gmail.com>
2023-10-26 14:09:30 -0400

    module name changed to flow_logging

--- logging/examples/flow_logs/main.tf
+++ logging/examples/flow_logs/main.tf
@@ -1,7 +1,7 @@
 # Copyright (c) 2023 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-module "vision_logging" {
+module "flow_logging" {
   source               = "../../"
   logging_configuration = var.logging_configuration
   compartments_dependency = var.automation_config != null ? merge([for o in data.oci_objectstorage_object.compartments : jsondecode(o.content)]...) : null
