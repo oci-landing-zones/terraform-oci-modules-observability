aea0fa7eef85f20365472e0307fbe1bc1b696954
Andre Correa <andre.correa@oracle.com>
2026-08-21 09:42:50 -0300

    feat: new preconfigured alarms

--- alarms/preconfigured_alarms.tf
+++ alarms/preconfigured_alarms.tf
@@ -3,7 +3,8 @@
 
 locals {
   preconfigured_alarms = {
-    high-cpu-alarm = {
+    # Compute Alarms
+    high-cpu-alarm = { # deprecated, use COMPUTE-HIGH-CPU-ALARM-WARNING or COMPUTE-HIGH-CPU-ALARM-CRITICAL
       namespace                           = "oci_computeagent"
       query                               = "CpuUtilization[1m].mean() > 80"
       severity                            = "CRITICAL"
@@ -11,7 +12,23 @@ locals {
       pending_duration                    = "PT5M"
       repeat_notification_critical_alarms = "PT4H"
     }
-    instance-status-alarm = {
+    COMPUTE-HIGH-CPU-ALARM-WARNING = {
+      namespace                           = "oci_computeagent"
+      query                               = "CpuUtilization[1m].mean() > 80"
+      severity                            = "WARNING"
+      message_format                      = "PRETTY_JSON"
+      pending_duration                    = "PT5M"
+      repeat_notification_critical_alarms = null
+    }
+    COMPUTE-HIGH-CPU-ALARM-CRITICAL = {
+      namespace                           = "oci_computeagent"
+      query                               = "CpuUtilization[1m].mean() > 90"
+      severity                            = "CRITICAL"
+      message_format                      = "PRETTY_JSON"
+      pending_duration                    = "PT5M"
+      repeat_notification_critical_alarms = "PT4H"
+    }
+    instance-status-alarm = { # deprecated, use COMPUTE-VM-STATUS-ALARM-CRITICAL
       namespace                           = "oci_compute_infrastructure_health"
       query                               = "instance_status[1m].count() == 1"
       severity                            = "CRITICAL"
@@ -19,7 +36,15 @@ locals {
       pending_duration                    = "PT5M"
       repeat_notification_critical_alarms = "PT4H"
     }
-    vm-maintenance-alarm = {
+    COMPUTE-VM-STATUS-ALARM-CRITICAL = { # Only for VM instances.
+      namespace                           = "oci_compute_infrastructure_health"
+      query                               = "instance_status[1m].count() == 1"
+      severity                            = "CRITICAL"
+      message_format                      = "PRETTY_JSON"
+      pending_duration                    = "PT5M"
+      repeat_notification_critical_alarms = "PT4H"
+    }
+    vm-maintenance-alarm = { # deprecated, use COMPUTE-MAINTENANCE-ALARM-WARNING
       namespace                           = "oci_compute_infrastructure_health"
       query                               = "maintenance_status[1m].count() == 1"
       severity                            = "CRITICAL"
@@ -27,7 +52,15 @@ locals {
       pending_duration                    = "PT5M"
       repeat_notification_critical_alarms = "PT4H"
     }
-    bare-metal-unhealthy-alarm = {
+    COMPUTE-MAINTENANCE-ALARM-WARNING = { # To both VMs and bare metal instances.
+      namespace                           = "oci_compute_infrastructure_health"
+      query                               = "maintenance_status[1m].count() == 1"
+      severity                            = "WARNING"
+      message_format                      = "PRETTY_JSON"
+      pending_duration                    = "PT5M"
+      repeat_notification_critical_alarms = null
+    }
+    bare-metal-unhealthy-alarm = { # deprecated, use COMPUTE-BARE-METAL-HEALTH-ALARM-CRITICAL
       namespace                           = "oci_compute_infrastructure_health"
       query                               = "health_status[1m].count() == 1"
       severity                            = "CRITICAL"
@@ -35,7 +68,15 @@ locals {
       pending_duration                    = "PT5M"
       repeat_notification_critical_alarms = "PT4H"
     }
-    high-memory-alarm = {
+    COMPUTE-BARE-METAL-HEALTH-ALARM-CRITICAL = { # Only for bare metal instances.
+      namespace                           = "oci_compute_infrastructure_health"
+      query                               = "health_status[1m].count() == 1"
+      severity                            = "CRITICAL"
+      message_format                      = "PRETTY_JSON"
+      pending_duration                    = "PT5M"
+      repeat_notification_critical_alarms = "PT4H"
+    }
+    high-memory-alarm = { # deprecated, use COMPUTE-HIGH-MEMORY-ALARM-WARNING or COMPUTE-HIGH-MEMORY-ALARM-CRITICAL
       namespace                           = "oci_computeagent"
       query                               = "MemoryUtilization[1m].mean() > 80"
       severity                            = "CRITICAL"
@@ -43,7 +84,25 @@ locals {
       pending_duration                    = "PT5M"
       repeat_notification_critical_alarms = "PT4H"
     }
-    adb-cpu-alarm = {
+    COMPUTE-HIGH-MEMORY-ALARM-WARNING = {
+      namespace                           = "oci_computeagent"
+      query                               = "MemoryUtilization[1m].mean() > 80"
+      severity                            = "WARNING"
+      message_format                      = "PRETTY_JSON"
+      pending_duration                    = "PT5M"
+      repeat_notification_critical_alarms = null
+    }
+    COMPUTE-HIGH-MEMORY-ALARM-CRITICAL = {
+      namespace                           = "oci_computeagent"
+      query                               = "MemoryUtilization[1m].mean() > 90"
+      severity                            = "CRITICAL"
+      message_format                      = "PRETTY_JSON"
+      pending_duration                    = "PT5M"
+      repeat_notification_critical_alarms = "PT4H"
+    }
+
+    # Autonomous Database Alarms
+    adb-cpu-alarm = { # deprecated, use ADB-HIGH-CPU-ALARM-WARNING or ADB-HIGH-CPU-ALARM-CRITICAL
       namespace                           = "oci_autonomous_database"
       query                               = "CpuUtilization[1m].mean() > 80"
       severity                            = "CRITICAL"
@@ -51,7 +110,25 @@ locals {
       pending_duration                    = "PT5M"
       repeat_notification_critical_alarms = "PT4H"
     }
-    adb-storage-alarm = {
+    ADB-HIGH-CPU-ALARM-WARNING = {
+      namespace                           = "oci_autonomous_database"
+      query                               = "CpuUtilization[5m].mean() > 80"
+      severity                            = "WARNING"
+      message_format                      = "PRETTY_JSON"
+      pending_duration                    = "PT5M"
+      repeat_notification_critical_alarms = null
+      freeform_tags                       = { provider = "DBM" }
+    }
+    ADB-HIGH-CPU-ALARM-CRITICAL = {
+      namespace                           = "oci_autonomous_database"
+      query                               = "CpuUtilization[5m].mean() > 85"
+      severity                            = "CRITICAL"
+      message_format                      = "PRETTY_JSON"
+      pending_duration                    = "PT5M"
+      repeat_notification_critical_alarms = "PT4H"
+      freeform_tags                       = { provider = "DBM" }
+    }
+    adb-storage-alarm = { # deprecated, use ADB-HIGH-STORAGE-ALARM-WARNING or ADB-HIGH-STORAGE-ALARM-CRITICAL
       namespace                           = "oci_autonomous_database"
       query                               = "StorageUtilization[1m].mean() > 80"
       severity                            = "CRITICAL"
@@ -59,7 +136,63 @@ locals {
       pending_duration                    = "PT5M"
       repeat_notification_critical_alarms = "PT4H"
     }
-    vpn-status-alarm = {
+    ADB-HIGH-STORAGE-ALARM-WARNING = {
+      namespace                           = "oci_autonomous_database"
+      query                               = "StorageUtilization[30m].mean() > 75"
+      severity                            = "WARNING"
+      message_format                      = "PRETTY_JSON"
+      pending_duration                    = "PT5M"
+      repeat_notification_critical_alarms = null
+      freeform_tags                       = { provider = "DBM" }
+    }
+    ADB-HIGH-STORAGE-ALARM-CRITICAL = {
+      namespace                           = "oci_autonomous_database"
+      query                               = "StorageUtilization[30m].mean() > 85"
+      severity                            = "CRITICAL"
+      message_format                      = "PRETTY_JSON"
+      pending_duration                    = "PT5M"
+      repeat_notification_critical_alarms = "PT4H"
+      freeform_tags                       = { provider = "DBM" }
+
+    }
+    ADB-FAILED-LOGINS-ALARM-WARNING = {
+      namespace                           = "oci_autonomous_database"
+      query                               = "FailedLogons[5m].mean() > 20"
+      severity                            = "WARNING"
+      message_format                      = "PRETTY_JSON"
+      pending_duration                    = "PT5M"
+      repeat_notification_critical_alarms = null
+      freeform_tags                       = { provider = "DBM" }
+    }
+    ADB-FAILED-LOGINS-ALARM-CRITICAL = {
+      namespace                           = "oci_autonomous_database"
+      query                               = "FailedLogons[5m].mean() > 30"
+      severity                            = "CRITICAL"
+      message_format                      = "PRETTY_JSON"
+      pending_duration                    = "PT5M"
+      repeat_notification_critical_alarms = "PT4H"
+      freeform_tags                       = { provider = "DBM" }
+    }
+    ADB-MONITORING-AVAILABILITY-ALARM-CRITICAL = {
+      namespace                           = "oci_autonomous_database"
+      query                               = "DatabaseAvailability[10m].absent()"
+      severity                            = "CRITICAL"
+      message_format                      = "PRETTY_JSON"
+      pending_duration                    = "PT1M"
+      repeat_notification_critical_alarms = "PT4H"
+      freeform_tags                       = { provider = "DBM" }
+    }
+    ADB-SESSIONS-ALARM-WARNING = {
+      namespace                           = "oci_autonomous_database"
+      query                               = "Sessions[15m].mean() > 30"
+      severity                            = "WARNING"
+      message_format                      = "PRETTY_JSON"
+      pending_duration                    = "PT5M"
+      repeat_notification_critical_alarms = null
+      freeform_tags                       = { provider = "DBM" }
+    }
+    # Network Alarms
+    vpn-status-alarm = { # deprecated, use NETWORK-VPN-STATUS-ALARM-CRITICAL
       namespace                           = "oci_vpn"
       query                               = "TunnelState[1m].mean() == 0"
       severity                            = "CRITICAL"
@@ -67,7 +200,15 @@ locals {
       pending_duration                    = "PT5M"
       repeat_notification_critical_alarms = "PT4H"
     }
-    fast-connect-status-alarm = {
+    NETWORK-VPN-STATUS-ALARM-CRITICAL = {
+      namespace                           = "oci_network"
+      query                               = "TunnelState[1m].mean() == 0"
+      severity                            = "CRITICAL"
+      message_format                      = "PRETTY_JSON"
+      pending_duration                    = "PT5M"
+      repeat_notification_critical_alarms = "PT4H"
+    }
+    fast-connect-status-alarm = { # deprecated, use NETWORK-FAST-CONNECT-STATUS-ALARM-CRITICAL
       namespace                           = "oci_fastconnect"
       query                               = "ConnectionState[1m].mean() == 0"
       severity                            = "CRITICAL"
@@ -75,47 +216,104 @@ locals {
       pending_duration                    = "PT5M"
       repeat_notification_critical_alarms = "PT4H"
     }
-    exadata-vm-cluster-high-cpu-alarm = {
+    NETWORK-FAST-CONNECT-STATUS-ALARM-CRITICAL = {
+      namespace                           = "oci_fastconnect"
+      query                               = "ConnectionState[1m].mean() == 0"
+      severity                            = "CRITICAL"
+      message_format                      = "PRETTY_JSON"
+      pending_duration                    = "PT5M"
+      repeat_notification_critical_alarms = "PT4H"
+    }
+    NETWORK-VNIC-CONNECTION-TRACKING-ALARM-CRITICAL = {
+      namespace                           = "oci_vcn"
+      query                               = "VnicConntrackUtilPercent[5m].groupBy(resourceId).max() >= 90"
+      severity                            = "CRITICAL"
+      message_format                      = "PRETTY_JSON"
+      pending_duration                    = "PT5M"
+      repeat_notification_critical_alarms = "PT4H"  
+    }
+    # Exadata Alarms
+    EXADATA-VM-CLUSTER-HIGH-CPU-ALARM-WARNING = {
       namespace                           = "oci_database_cluster"
       query                               = "CpuUtilization[1m].mean() > 80"
+      severity                            = "WARNING"
+      message_format                      = "PRETTY_JSON"
+      pending_duration                    = "PT5M"
+      repeat_notification_critical_alarms = null
+    }
+    EXADATA-VM-CLUSTER-HIGH-CPU-ALARM-CRITICAL = {
+      namespace                           = "oci_database_cluster"
+      query                               = "CpuUtilization[1m].mean() > 90"
       severity                            = "CRITICAL"
       message_format                      = "PRETTY_JSON"
       pending_duration                    = "PT5M"
       repeat_notification_critical_alarms = "PT4H"
     }
-    exadata-vm-cluster-high-memory-alarm = {
+    EXADATA-VM-CLUSTER-HIGH-MEMORY-ALARM-WARNING = {
       namespace                           = "oci_database_cluster"
       query                               = "MemoryUtilization[1m].mean() > 80"
-      severity                            = "CRITICAL"
+      severity                            = "WARNING"
       message_format                      = "PRETTY_JSON"
       pending_duration                    = "PT5M"
-      repeat_notification_critical_alarms = "PT4H"
+      repeat_notification_critical_alarms = null
     }
-    exadata-vm-cluster-high-filesystem-utilization-alarm = {
+    EXADATA-VM-CLUSTER-HIGH-MEMORY-ALARM-CRITICAL = {
       namespace                           = "oci_database_cluster"
-      query                               = "FilesystemUtilization[1m].mean() > 80"
+      query                               = "MemoryUtilization[1m].mean() > 90"
       severity                            = "CRITICAL"
       message_format                      = "PRETTY_JSON"
       pending_duration                    = "PT5M"
       repeat_notification_critical_alarms = "PT4H"
     }
-    exadata-vm-cluster-high-asm-diskgroup-utilization-alarm = {
+    EXADATA-VM-CLUSTER-HIGH-FILESYSTEM-UTILIZATION-ALARM-WARNING = {
       namespace                           = "oci_database_cluster"
-      query                               = "ASMDiskgroupUtilization[10m].mean() > 80"
-      severity                            = "CRITICAL"
+      query                               = "FilesystemUtilization[30m].mean() > 75"
+      severity                            = "WARNING"
       message_format                      = "PRETTY_JSON"
       pending_duration                    = "PT5M"
-      repeat_notification_critical_alarms = "PT4H"
+      repeat_notification_critical_alarms = null
     }
-    exadata-vm-cluster-high-swap-utilization-alarm = {
+    EXADATA-VM-CLUSTER-HIGH-FILESYSTEM-UTILIZATION-ALARM-CRITICAL = {
       namespace                           = "oci_database_cluster"
-      query                               = "SwapUtilization[1m].mean() > 80"
+      query                               = "FilesystemUtilization[30m].mean() > 85"
       severity                            = "CRITICAL"
       message_format                      = "PRETTY_JSON"
       pending_duration                    = "PT5M"
       repeat_notification_critical_alarms = "PT4H"
     }
-    exadata-vm-cluster-node-status-alarm = {
+    EXADATA-VM-CLUSTER-HIGH-ASM-DISKGROUP-UTILIZATION-ALARM-WARNING = {
+      namespace                           = "oci_database_cluster"
+      query                               = "ASMDiskgroupUtilization[30m].mean() > 75"
+      severity                            = "WARNING"
+      message_format                      = "PRETTY_JSON"
+      pending_duration                    = "PT5M"
+      repeat_notification_critical_alarms = null
+    }
+    EXADATA-VM-CLUSTER-HIGH-ASM-DISKGROUP-UTILIZATION-ALARM-CRITICAL = {
+      namespace                           = "oci_database_cluster"
+      query                               = "ASMDiskgroupUtilization[30m].mean() > 85"
+      severity                            = "CRITICAL"
+      message_format                      = "PRETTY_JSON"
+      pending_duration                    = "PT5M"
+      repeat_notification_critical_alarms = "PT4H"
+    }
+    EXADATA-VM-CLUSTER-HIGH-SWAP-UTILIZATION-ALARM-WARNING = {
+      namespace                           = "oci_database_cluster"
+      query                               = "SwapUtilization[5m].mean() > 75"
+      severity                            = "WARNING"
+      message_format                      = "PRETTY_JSON"
+      pending_duration                    = "PT5M"
+      repeat_notification_critical_alarms = null
+    }
+    EXADATA-VM-CLUSTER-HIGH-SWAP-UTILIZATION-ALARM-CRITICAL = {
+      namespace                           = "oci_database_cluster"
+      query                               = "SwapUtilization[5m].mean() > 85"
+      severity                            = "CRITICAL"
+      message_format                      = "PRETTY_JSON"
+      pending_duration                    = "PT5M"
+      repeat_notification_critical_alarms = "PT4H"
+    }
+    EXADATA-VM-CLUSTER-NODE-STATUS-ALARM-CRITICAL = {
       namespace                           = "oci_database_cluster"
       query                               = "NodeStatus[1m].mean() == 0"
       severity                            = "CRITICAL"
@@ -123,17 +321,33 @@ locals {
       pending_duration                    = "PT5M"
       repeat_notification_critical_alarms = "PT4H"
     }
-    exadata-database-high-cpu-alarm = {
+    EXADATA-DATABASE-HIGH-CPU-ALARM-WARNING = {
       namespace                           = "oci_database"
-      query                               = "CpuUtilization[5m].mean() > 80"
+      query                               = "CpuUtilization[1m].mean() > 80"
+      severity                            = "WARNING"
+      message_format                      = "PRETTY_JSON"
+      pending_duration                    = "PT5M"
+      repeat_notification_critical_alarms = null
+    }
+    EXADATA-DATABASE-HIGH-CPU-ALARM-CRITICAL = {
+      namespace                           = "oci_database"
+      query                               = "CpuUtilization[1m].mean() > 90"
       severity                            = "CRITICAL"
       message_format                      = "PRETTY_JSON"
       pending_duration                    = "PT5M"
       repeat_notification_critical_alarms = "PT4H"
     }
-    exadata-database-high-storage-utilization-alarm = {
+    EXADATA-DATABASE-HIGH-STORAGE-UTILIZATION-ALARM-WARNING = {
       namespace                           = "oci_database"
-      query                               = "StorageUtilization[1h].mean() > 80"
+      query                               = "StorageUtilization[30m].mean() > 75"
+      severity                            = "WARNING"
+      message_format                      = "PRETTY_JSON"
+      pending_duration                    = "PT5M"
+      repeat_notification_critical_alarms = null
+    }
+    EXADATA-DATABASE-HIGH-STORAGE-UTILIZATION-ALARM-CRITICAL = {
+      namespace                           = "oci_database"
+      query                               = "StorageUtilization[30m].mean() > 85"
       severity                            = "CRITICAL"
       message_format                      = "PRETTY_JSON"
       pending_duration                    = "PT5M"
